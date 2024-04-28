a9c7f399305c2f822d14a9b69fb1721a922e14a2
Aiden-Jung <hyunseok0314@gmail.com>
2024-04-28 18:26:55 +0900

    feat: #14 add userinfo dto for oauth2

---
+++ src/main/java/ssafy/lambda/config/security/oauth2/userinfo/OAuth2UserInfo.java
@@ -0,0 +1,12 @@
+package ssafy.lambda.config.security.oauth2.userinfo;
+
+import ssafy.lambda.member.entity.SocialType;
+
+public interface OAuth2UserInfo {
+
+    SocialType getSocial();
+
+    String getEmail();
+
+    String getName();
+}
