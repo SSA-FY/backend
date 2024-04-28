043aa642bd504afee0678824a355451e4daa3745
q0uu0p <q0_u_0p@naver.com>
2024-04-29 01:27:36 +0900

    feat: VoteComment 관련 dto 추가

---
+++ src/main/java/ssafy/lambda/vote/dto/ResponseCommentDto.java
@@ -0,0 +1,17 @@
+package ssafy.lambda.vote.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ResponseCommentDto {
+
+    Long commentId;
+    String writer;
+    String content;
+    LocalDateTime time;
+
+
+}
