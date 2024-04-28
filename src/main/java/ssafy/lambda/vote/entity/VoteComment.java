6c89db2cd7f560fe80a06f9b71c860222453ae6a
q0uu0p <q0_u_0p@naver.com>
2024-04-29 01:26:23 +0900

    feat: VoteComment 엔티티 추가

---
+++ src/main/java/ssafy/lambda/vote/entity/VoteComment.java
@@ -0,0 +1,50 @@
+package ssafy.lambda.vote.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import ssafy.lambda.member.entity.Member;
+
+@Entity
+@Getter
+public class VoteComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expired_vote_id")
+    private ExpiredVote expiredVote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected VoteComment() {
+
+    }
+
+    @Builder
+    public VoteComment(String content, ExpiredVote expiredVote, Member member) {
+        this.content = content;
+        this.createAt = LocalDateTime.now();
+        this.expiredVote = expiredVote;
+        this.member = member;
+    }
+}
