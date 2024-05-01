ba7e4a264da640d2eb5873fac30c005bf1bd29f0
dino9881 <whdghthdms44@gmail.com>
2024-05-01 21:04:08 +0900

    feat: Team entity manger 추가

--- src/main/java/ssafy/lambda/team/entity/Team.java
+++ src/main/java/ssafy/lambda/team/entity/Team.java
@@ -2,9 +2,12 @@ package ssafy.lambda.team.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -12,6 +15,7 @@ import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.membership.entity.Membership;
 
 @Entity
@@ -38,16 +42,21 @@ public class Team {
     @OneToMany(mappedBy = "team", orphanRemoval = true)
     private List<Membership> memberships = new ArrayList<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, updatable = false, name = "manager_id")
+    private Member manager;
+
     protected Team() {
     }
 
     @Builder
-    public Team(Long teamId, String teamName, String description) {
+    public Team(Long teamId, String teamName, String description, Member manager) {
         this.teamId = teamId;
         this.teamName = teamName;
         this.description = description;
         this.createdAt = LocalDateTime.now();
         this.participants = 0;
+        this.manager = manager;
     }
 
     public void update(String teamName, String description) {
