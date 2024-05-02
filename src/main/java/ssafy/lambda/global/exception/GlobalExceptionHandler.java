79265338ea6457f8d322ae098683ea0b3567d187
dino9881 <whdghthdms44@gmail.com>
2024-05-02 09:36:52 +0900

    feat: GlobalExceptionHandler 에 Team 관련 예외 추가
    - Team domain 에서 발생하는 예외를 처리할 수 있도록 추가하였습니다.
    - MemberNotFoundException 을 처리하는 메소드명이 중복되어 수정하였습니다.
    
    issue : #15

--- src/main/java/ssafy/lambda/global/exception/GlobalExceptionHandler.java
+++ src/main/java/ssafy/lambda/global/exception/GlobalExceptionHandler.java
@@ -8,14 +8,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ssafy.lambda.global.response.Response;
 import ssafy.lambda.member.exception.MemberNotFoundException;
+import ssafy.lambda.team.exception.TeamNotFoundException;
+import ssafy.lambda.team.exception.TeamUnauthorizedException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MemberNotFoundException.class)
     @ResponseBody
-    public ResponseEntity<Response> handleNotFoundException(MemberNotFoundException e) {
+    public ResponseEntity<Response> handleMembersNotFoundException(MemberNotFoundException e) {
         return Response.res(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(TeamNotFoundException.class)
+    @ResponseBody
+    public ResponseEntity<Response> handleTeamNotFoundException(TeamNotFoundException e) {
+        return Response.res(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(TeamUnauthorizedException.class)
+    @ResponseBody
+    public ResponseEntity<Response> handleTeamUnauthorizedException(TeamUnauthorizedException e) {
+        return Response.res(HttpStatus.UNAUTHORIZED, e.getMessage());
     }
 
 }
