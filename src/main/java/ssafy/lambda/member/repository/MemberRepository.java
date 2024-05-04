36782358c1e1a1485869c33f95cb1b51b12d1c3a
Aiden-Jung <hyunseok0314@gmail.com>
2024-05-05 08:03:37 +0900

    feat: #37 add findAllMemberByTeamId and find AllTeamByMemberId to Member and Team, respectively

--- src/main/java/ssafy/lambda/member/repository/MemberRepository.java
+++ src/main/java/ssafy/lambda/member/repository/MemberRepository.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ssafy.lambda.member.entity.Member;
+import ssafy.lambda.membership.entity.Membership;
 
 
 @Repository
@@ -18,4 +20,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Override
     @EntityGraph(attributePaths = {"memberships"})
     List<Member> findAll();
+
+
+    @Query("SELECT member from Member member LEFT JOIN member.memberships membership LEFT JOIN membership.team team where team.id = :teamId")
+    List<Member> findAllByTeamId(Long teamId);
 }
--- src/main/java/ssafy/lambda/team/repository/TeamRepository.java
+++ src/main/java/ssafy/lambda/team/repository/TeamRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.team.entity.Team;
 
 @Repository
@@ -17,4 +19,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @Override
     @EntityGraph(attributePaths = {"memberships"})
     List<Team> findAll();
+
+    @Query("SELECT team from Team team LEFT JOIN team.memberships membership LEFT JOIN membership.member member where member.id = :memberId")
+    List<Team> findAllByMemberId(Long memberId);
 }
