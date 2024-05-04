4a9dc20d5147db993bf1f3c95846607b80976966
dino9881 <whdghthdms44@gmail.com>
2024-05-04 18:05:56 +0900

    feat: Membership Entity 수정
    - BaseEntity 를 상속받도록 변경
    - nickname 필드 추가
    - column 명 수정
    
    issue : #34

--- src/main/java/ssafy/lambda/membership/entity/Membership.java
+++ src/main/java/ssafy/lambda/membership/entity/Membership.java
@@ -1,5 +1,6 @@
 package ssafy.lambda.membership.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,32 +10,39 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import ssafy.lambda.global.common.BaseEntity;
 import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.team.entity.Team;
 
 @Getter
 @Entity
-public class Membership {
+public class Membership extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @Column(name = "membership_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "member_id")
-  private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "team_id")
-  private Team team;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
-  protected Membership() {
-  }
+    @Setter
+    @Column(name = "nickname")
+    private String nickname;
 
-  @Builder
-  public Membership(Long id, Member member, Team team) {
-    this.id = id;
-    this.member = member;
-    this.team = team;
-  }
+    protected Membership() {
+    }
+
+    @Builder
+    public Membership(Member member, Team team, String nickname) {
+        this.member = member;
+        this.team = team;
+        this.nickname = nickname;
+    }
 }
