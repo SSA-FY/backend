4151e1fff8d8aa00f67fd7dfa6f70c371bb0d353
SeungCheol <ys4512558@naver.com>
2024-05-08 22:39:02 +0900

    feat: Notification 엔티티 수정

--- src/main/java/ssafy/lambda/notification/entity/NotificationDetail/ExpiredVoteNotification.java
+++ src/main/java/ssafy/lambda/notification/entity/NotificationDetail/ExpiredVoteNotification.java
@@ -1,8 +1,10 @@
 package ssafy.lambda.notification.entity.NotificationDetail;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import ssafy.lambda.board.entity.ExpiredVote;
+import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.notification.entity.Notification;
 
 @Entity
@@ -13,4 +15,14 @@ public class ExpiredVoteNotification extends Notification {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "expired_vote_id")
     private ExpiredVote expiredVote;
+
+    protected ExpiredVoteNotification(){
+
+    }
+
+    @Builder
+    public ExpiredVoteNotification(Long id, Member member, ExpiredVote expiredVote) {
+        super(id, member);
+        this.expiredVote = expiredVote;
+    }
 }
