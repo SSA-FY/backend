1bc7b897dabf1124bed13ca84defbdc729c4a663
왕승철 <ys4512558@naver.com>
2024-05-10 01:32:20 +0900

    feat: NotificationService개발
    - Repository로 부터 받은 데이터를 DTO로 변환하여 반환하도록
    - 각 DTO에 따라 Dto 생성 메서드를 통해 컨버팅하도록 개발

--- src/main/java/ssafy/lambda/notification/service/NotificationServiceImpl.java
+++ src/main/java/ssafy/lambda/notification/service/NotificationServiceImpl.java
@@ -1,21 +1,31 @@
 package ssafy.lambda.notification.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ssafy.lambda.board.entity.ExpiredVote;
 import ssafy.lambda.invitation.entity.Invitation;
 import ssafy.lambda.member.entity.Member;
+import ssafy.lambda.notification.dto.ResponseNotificationDto;
+import ssafy.lambda.notification.entity.Notification;
+import ssafy.lambda.notification.entity.NotificationDetail.ExpiredVoteNotification;
 import ssafy.lambda.notification.entity.NotificationDetail.InvitationNotification;
 import ssafy.lambda.notification.entity.NotificationDetail.VoteNotification;
 import ssafy.lambda.notification.repository.NotificationRepository;
 import ssafy.lambda.vote.entity.Vote;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
+
     @Override
+    @Transactional
     public void createVoteNotification(Member member, Vote vote){
         VoteNotification voteNotification = VoteNotification.builder()
                                                             .member(member)
@@ -25,11 +35,13 @@ public class NotificationServiceImpl implements NotificationService {
     }
 
     @Override
+    @Transactional
     public void createExpiredVoteNotification(Member member, ExpiredVote expiredVote) {
 
     }
 
     @Override
+    @Transactional
     public void createInvitationNotification(Member member, Invitation invitaionId) {
         InvitationNotification invitationNotification = InvitationNotification.builder()
                                                                               .invitation(invitaionId)
@@ -37,4 +49,40 @@ public class NotificationServiceImpl implements NotificationService {
                                                                               .build();
         notificationRepository.save(invitationNotification);
     }
+
+    /**
+     * Transactional 선언 이유 :
+     * 페이징으로 가져온 결과에서 형변환 로직에서
+     * VoteNotification -> Vote참조 시 같은 트랜잭션 내에서만 참조 가능
+     * @param member
+     * @param pageable
+     * @return
+     */
+    @Override
+    @Transactional
+    public List<ResponseNotificationDto> getNotificationListByMember(Member member, Pageable pageable) {
+        Page<Notification> notifications = notificationRepository.findNotificationByMember(member, pageable);
+
+        //TODO: 각 하위타입에 따라 DTO로 변환해서 반환
+        Page<ResponseNotificationDto> notificationDtos = notifications.map(notification -> {
+            if (notification instanceof VoteNotification) {
+                VoteNotification voteNotification = (VoteNotification) notification;
+                return ResponseNotificationDto.NotificationToVoteDto(
+                        voteNotification.getVote()
+                                        .getId(),
+                        voteNotification.getVote()
+                                        .getContent(),
+                        notificationRepository.findVoteNotificationInfoByVoteAndMember(voteNotification.getVote(), voteNotification.getMember()));
+            } else if (notification instanceof ExpiredVoteNotification) {
+                return null;
+            } else if (notification instanceof InvitationNotification) {
+                return null;
+            } else {
+                return null;
+            }
+        });
+
+        return notificationDtos.getContent();
+    }
+
 }
