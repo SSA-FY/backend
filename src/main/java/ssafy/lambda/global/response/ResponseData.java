6e1a0acca6f284f7e8be85154556948e91f9e733
dino9881 <whdghthdms44@gmail.com>
2024-05-09 14:51:55 +0900

    feat: ResponseData class 생성
    issue : #47

---
+++ src/main/java/ssafy/lambda/global/response/ResponseData.java
@@ -0,0 +1,24 @@
+package ssafy.lambda.global.response;
+
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class ResponseData<T> extends Response {
+
+    private final T data;
+
+    public ResponseData(HttpStatus status, String message, T data) {
+        super(status, message);
+        this.data = data;
+    }
+
+    public static <T> ResponseEntity res(HttpStatus status, String message,
+        T data) {
+        return ResponseEntity
+            .status(status)
+            .body(new ResponseData<>(status, message, data));
+    }
+}
