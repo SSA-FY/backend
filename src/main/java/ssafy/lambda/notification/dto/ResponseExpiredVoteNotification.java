acb5c3a9ed0f8fddf833e6b8eefdb4e91c101ff1
왕승철 <ys4512558@naver.com>
2024-05-10 01:29:18 +0900

    feat: ResponseDto하위 타입을 묶어주는 Dto 개발
    - 투표관련, 투표완료관련, 초대 관련 Dto를  ResponseNotificationDto로 묶어서 리스트로 반환

--- src/main/java/ssafy/lambda/notification/dto/ResponseExpiredVoteNotification.java
+++ src/main/java/ssafy/lambda/notification/dto/ResponseExpiredVoteNotification.java
@@ -4,14 +4,17 @@ import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 
 @Data
-public class ResponseExpiredVoteNotification extends ResponseNotification{
+public class ResponseExpiredVoteNotification{
+
+    private static final String TITLE = "투표가 종료되었어요!";
+
     private Long expiredVoteId;
 
     private String expiredVoteTitle;
 
     @QueryProjection
-    public ResponseExpiredVoteNotification(Long expiredVoteId, String content) {
-        super("투표가 종료되었어요!", content);
+    public ResponseExpiredVoteNotification(Long expiredVoteId, String expiredVoteTitle) {
         this.expiredVoteId = expiredVoteId;
+        this.expiredVoteTitle = expiredVoteTitle;
     }
 }
--- src/main/java/ssafy/lambda/notification/dto/ResponseVoteNotification.java
+++ src/main/java/ssafy/lambda/notification/dto/ResponseVoteNotification.java
@@ -8,20 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class ResponseVoteNotification extends ResponseNotification{
+public class ResponseVoteNotification{
+
+    private static final String TITLE = "누군가 나를 뽑았어요!";
 
     private Long voteId;
+
+    private String voteTitle;
 
     //내가 뽑힌 투표 정보
     private List<VoteInfoItem> voteInfoItems;
 
+
     @QueryProjection
-    public ResponseVoteNotification(Long voteId, String content) {
-        super("누군가 나를 뽑았어요!", content);
+    public ResponseVoteNotification(Long voteId, String voteTitle, List<VoteInfoItem> voteInfoItems) {
         this.voteId = voteId;
-        this.voteInfoItems = new ArrayList<>();
-    }
-    public void addVoteInfoItems(VoteInfoItem voteInfoItem){
-        this.voteInfoItems.add(voteInfoItem);
+        this.voteTitle = voteTitle;
+        this.voteInfoItems = voteInfoItems;
     }
 }
