685ec07961d856fd403bf2051ad224f05e9a35c9
dino9881 <whdghthdms44@gmail.com>
2024-05-10 11:56:59 +0900

    feat: ApiErrorResponse 커스텀 어노테이션 생성
    issue : #47

---
+++ src/main/java/ssafy/lambda/global/annotation/ApiErrorResponse.java
@@ -0,0 +1,20 @@
+package ssafy.lambda.global.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import ssafy.lambda.global.response.ApiError;
+
+/**
+ * Controller 에 붙여서 사용하는 API 에서 생길수 있는 에러들을 쉽게 정의할 수 있는 커스텀 어노테이션 입니다.
+ *
+ * @ApiErrorResponse([]) => 배열 안에 ApiErrorResponse Enum 을 넣으면 작동합니다.
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiErrorResponse {
+
+    ApiError[] value();
+}
