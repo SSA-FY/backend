d3c55b6f28e6ee806e19ed80bf75dc56de2c9aef
dino9881 <whdghthdms44@gmail.com>
2024-05-10 12:08:04 +0900

    docs: 주석 오타 수정
    issue : #47

--- src/main/java/ssafy/lambda/global/annotation/ApiErrorResponse.java
+++ src/main/java/ssafy/lambda/global/annotation/ApiErrorResponse.java
@@ -9,7 +9,7 @@ import ssafy.lambda.global.response.ApiError;
 /**
  * Controller 에 붙여서 사용하는 API 에서 생길수 있는 에러들을 쉽게 정의할 수 있는 커스텀 어노테이션 입니다.
  *
- * @ApiErrorResponse([]) => 배열 안에 ApiErrorResponse Enum 을 넣으면 작동합니다.
+ * @ApiErrorResponse({)) => 배열 안에 ApiErrorResponse Enum 을 넣으면 작동합니다.
  */
 
 @Target(ElementType.METHOD)
