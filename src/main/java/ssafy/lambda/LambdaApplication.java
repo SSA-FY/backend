d6fb86ad29cbfc77980a29c57fa595c329ac013c
Aiden-Jung <hyunseok0314@gmail.com>
2024-06-09 18:57:32 +0900

    feat: #52 add cors config

--- src/main/java/ssafy/lambda/LambdaApplication.java
+++ src/main/java/ssafy/lambda/LambdaApplication.java
@@ -7,6 +7,10 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SecurityScheme(name = "token", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
@@ -17,4 +21,21 @@ public class LambdaApplication {
         SpringApplication.run(LambdaApplication.class, args);
     }
 
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOriginPatterns("http://localhost:5000/")
+                    .allowCredentials(true)
+                    .allowedMethods(
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name());
+        }
+    }
 }
