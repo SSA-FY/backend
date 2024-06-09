8235c00b4334e5e78ba5fe56585b8f60d14a640a
Aiden-Jung <hyunseok0314@gmail.com>
2024-06-09 19:01:55 +0900

    chore: #52 setup title and version for swagger

--- src/main/java/ssafy/lambda/LambdaApplication.java
+++ src/main/java/ssafy/lambda/LambdaApplication.java
@@ -3,6 +3,7 @@ package ssafy.lambda;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
@@ -12,7 +13,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(info = @Info(title = "Lambda API", version = "v1.0.0"), servers = {
+    @Server(url = "/", description = "Default Server URL")})
 @SecurityScheme(name = "token", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @SpringBootApplication
 public class LambdaApplication {
