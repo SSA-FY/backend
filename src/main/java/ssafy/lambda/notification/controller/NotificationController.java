cae6cc29df99ce4b959b0982b60d1616fb3ea320
왕승철 <ys4512558@naver.com>
2024-06-14 00:10:37 +0900

    feat: 알림 삭제 API 개발

--- src/main/java/ssafy/lambda/notification/controller/NotificationController.java
+++ src/main/java/ssafy/lambda/notification/controller/NotificationController.java
@@ -1,22 +1,25 @@
 package ssafy.lambda.notification.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ssafy.lambda.global.annotation.ApiErrorResponse;
+import ssafy.lambda.global.response.ApiError;
+import ssafy.lambda.global.response.dto.Response;
+import ssafy.lambda.global.response.dto.ResponseData;
 import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.member.service.MemberService;
 import ssafy.lambda.notification.dto.ResponseNotificationDto;
 import ssafy.lambda.notification.service.NotificationService;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 @Slf4j
@@ -31,12 +34,27 @@ public class NotificationController {
     private final MemberService memberService;
 
     @Operation(description = "알림 리스트 반환", summary = "알림 리스트 반환")
+    @ApiErrorResponse({ApiError.MemberNotFound})
     @GetMapping("/list")
     public ResponseEntity<List<ResponseNotificationDto>> getNotificationList(
             Authentication authentication,
             @RequestParam(name = "page") int page) {
         UUID memberId = UUID.fromString(authentication.getName());
         Member member = memberService.findMemberById(memberId);
-        return ResponseEntity.ok(notificationService.getNotificationListByMember(member, PageRequest.of(page, 10)));
+        return ResponseData.res(HttpStatus.OK, "알림 리스트", notificationService.getNotificationListByMember(member, PageRequest.of(page, 10)));
+    }
+
+    @Operation(description = "해당 알림을 삭제합니다.", summary = "알림 삭제")
+    @ApiErrorResponse({ApiError.NotificationNotFound, ApiError.UnauthorizedMember})
+    @DeleteMapping("/{notificationId}")
+    public ResponseEntity<HttpStatus> deleteNotification(
+            Authentication authentication,
+            @PathVariable("notificationId") Long notificationId) {
+        UUID memberId = UUID.fromString(authentication.getName());
+        Member member = memberService.findMemberById(memberId);
+        if (notificationService.deleteNotification(member, notificationId)) {
+            return Response.res(HttpStatus.NO_CONTENT, "알림을 삭제하였습니다.");
+        }
+        return Response.res(HttpStatus.NOT_FOUND, "존재하지 않는 알림입니다.");
     }
 }
