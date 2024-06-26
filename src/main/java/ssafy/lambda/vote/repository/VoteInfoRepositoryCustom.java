b23904c563b193f315a4f56672d3414d804db3bd
왕승철 <ys4512558@naver.com>
2024-06-26 18:00:47 +0900

    feat: 멤버를 통해 오늘 투표한 정보를 반환하는 Repository 메서드 개발

--- src/main/java/ssafy/lambda/vote/repository/VoteInfoRepositoryCustom.java
+++ src/main/java/ssafy/lambda/vote/repository/VoteInfoRepositoryCustom.java
@@ -2,6 +2,7 @@ package ssafy.lambda.vote.repository;
 
 import java.util.List;
 import ssafy.lambda.member.entity.Member;
+import ssafy.lambda.vote.dto.ResponseTodayVoteInfoDto;
 import ssafy.lambda.vote.entity.Vote;
 import ssafy.lambda.vote.entity.VoteInfo;
 
@@ -9,4 +10,5 @@ public interface VoteInfoRepositoryCustom {
 
     List<VoteInfo> findVoteInfoToMeListByMemberAndVote(Member member, Vote vote);
 
+    ResponseTodayVoteInfoDto findTodayVoteInfoByMember(Member member);
 }
