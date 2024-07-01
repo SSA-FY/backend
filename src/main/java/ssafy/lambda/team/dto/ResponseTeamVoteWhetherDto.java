650fd5a68f685d9eaa6b570d4260ec8c6fd518b9
dino9881 <whdghthdms44@gmail.com>
2024-07-01 14:35:24 +0900

    fix: team GET /team API 투표 여부 확인 추가

--- src/main/java/ssafy/lambda/team/dto/ResponseTeamVoteWhetherDto.java
+++ src/main/java/ssafy/lambda/team/dto/ResponseTeamVoteWhetherDto.java
@@ -6,22 +6,23 @@ import ssafy.lambda.team.entity.Team;
 
 @Data
 public class ResponseTeamVoteWhetherDto {
-    private Long teamId;
+
     private String teamName;
+    private String imgUrl;
     private Boolean voteWhether;
 
     @Builder
-    public ResponseTeamVoteWhetherDto(Long teamId, String teamName, Boolean voteWhether) {
-        this.teamId = teamId;
+    public ResponseTeamVoteWhetherDto(String teamName, Boolean voteWhether, String imgUrl) {
         this.teamName = teamName;
         this.voteWhether = voteWhether;
+        this.imgUrl = imgUrl;
     }
 
     public static ResponseTeamVoteWhetherDto teamToDto(Team team, boolean voteWhether) {
         return ResponseTeamVoteWhetherDto.builder()
-                                         .teamId(team.getTeamId())
                                          .teamName(team.getTeamName())
                                          .voteWhether(voteWhether)
+                                         .imgUrl(team.getImgUrl())
                                          .build();
     }
 }
