1c0fc646d1ec4bc57fa99c1784c123dab6aa908b
왕승철 <ys4512558@naver.com>
2024-07-09 23:48:23 +0900

    feat: ExpiredVote와 VoteNotification 연관관계 설정 및 isExpired 칼럼 추가
    - 투표가 만료되어 해당 투표가 삭제되는 경우 VoteNotification이 ExpiredVote와 연관관계를 가지도록 하기 위해 수정
    - isExpired칼럼의 상태에 따라 어떤 연관된 엔티티에서 정보를 가져올지 설정하기 위해 사용

--- src/main/java/ssafy/lambda/notification/entity/NotificationDetail/VoteNotification.java
+++ src/main/java/ssafy/lambda/notification/entity/NotificationDetail/VoteNotification.java
@@ -1,36 +1,42 @@
 package ssafy.lambda.notification.entity.NotificationDetail;
 
-import jakarta.persistence.DiscriminatorValue;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import lombok.Setter;
+import ssafy.lambda.board.entity.ExpiredVote;
 import ssafy.lambda.member.entity.Member;
 import ssafy.lambda.notification.entity.Notification;
 import ssafy.lambda.vote.entity.Vote;
 
 @Entity
 @Getter
+@Setter
 @DiscriminatorValue("Vote")
 public class VoteNotification extends Notification {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "vote_id")
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Vote vote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expired_vote_id")
+    private ExpiredVote expiredVote;
+
+    @Column(name = "is_expired")
+    private Boolean isExpired;
+
 
     protected VoteNotification() {
 
     }
 
     @Builder
-    public VoteNotification(Member member, Vote vote) {
+    public VoteNotification(Member member, Vote vote, ExpiredVote expiredVote, Boolean isExpired) {
         super(member);
         this.vote = vote;
+        this.expiredVote = expiredVote;
+        this.isExpired = isExpired;
     }
 
 }
